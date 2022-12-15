64b4df4d741f3888170b0dd5d59f46da29bc0f41
Liju Thomas <liju.t@thinkpalm.com>
2022-12-15 14:10:07 +0530

    Integrated project -resolved multiple files issue

--- src/components/AdEmployeeList.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function AdEmployeeList() {
-  return (
-    <div>AdEmployeeList</div>
-  )
-}
-
-export default AdEmployeeList
--- src/components/Emp-comp/Emp_details.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function Emp_details() {
-  return (
-    <div>Emp_details</div>
-  )
-}
-
-export default Emp_details
